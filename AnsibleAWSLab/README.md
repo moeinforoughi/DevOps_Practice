7e4096c6aaf9efd6f7a757494cc4c037f182fa66
moeinforoughi <mforoughi1379@gmail.com>
2025-02-22 19:07:48 +0330

    Adding AnsibleAWS Lab

---
+++ AnsibleAWSLab/README.md
@@ -0,0 +1,4 @@
+This lab make 1 controller node and 3 manage nodes
+After updating your OS and packges for 4 machines
+Install Ansible and Docker and Docker-compose and AnsibleAWS on your controller node 
+Your Lab ready start a Ansible project on controller node and the montitoring with AnsibleAWS on a Docker image  and connect this machine with manage nodes
---
+++ AnsibleAWSLab/bootstrap_t1.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+ update and upgrade os
+ apt-get update
+ apt-get upgrade -y
+ UBUNTU_CODENAME=jammy
+ wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+ echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+ sudo apt update && sudo apt install ansible
+ sudo apt install apt-transport-https ca-certificates curl software-properties-common
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+ apt-cache policy docker-ce
+ sudo apt install docker-ce
+ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
+ sudo systemctl status docker
+ docker-compose --version
+ ansiblle --version
+ ssh-keygen
+ sudo apt update && sudo apt upgrade
